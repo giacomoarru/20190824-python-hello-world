a59f6a9f3d2c74bc2dd0b4edef72d99e7ace20c6
Giacomo Arru <33087921+giacomoarru@users.noreply.github.com>
2019-08-24 18:25:15 +0100

    Add files via upload

---
+++ exercise1.py
@@ -0,0 +1,30 @@
+import random
+import sys
+import os
+
+# Giacomo 2019-08-24
+
+'''
+ D A T A   T Y P E S
+Numbers
+Strings
+Lists
+Tuples
+Dictionaries
+
+ O P E R A T O R S
++  -  *  /  %  **  //
+
+
+
+'''
+print("5 + 2 =", 5+2)
+print("5 - 2 =", 5-2)
+print("5 * 2 =", 5*2)
+print("5 / 2 =", 5/2)
+print("5 % 2 =", 5%2)
+print("5 ** 2 =", 5**2)
+print("5 // 2 =", 5//2)
+
+print("%s %s %s" % ("Hello", "General", "Kenobi"))
+
